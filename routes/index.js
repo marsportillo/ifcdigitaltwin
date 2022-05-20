4a675e24cbc74c4a8837b216050163a299d0dc3f
Marco Sportillo <msportillo@gmail.com>
2022-05-20 15:32:47 +0200

    Add the quick viewer route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ifc Digital Twin' });
+});
+
+router.get('/quickViewer', function(req, res, next) {
+  res.render('quickViewer', { title: 'Ifc Digital Twin - Quick Viewer' });
 });
 
 module.exports = router;
